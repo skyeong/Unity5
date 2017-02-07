2b729cabf6280f35711b2a988cba16097907271a
Sunghyon Kyeong <sunghyon.kyeong@gmail.com>
2017-02-08 00:46:48 +0900

    HW5

---
+++ hw5/Shooting_End/Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+	// static은 player나 enermy에 속하지 않고 class 자체에 속한다. 
+    public static int score;
+
+
+    Text text;
+
+
+    void Awake ()
+    {
+        text = GetComponent <Text> ();
+        score = 0;
+    }
+
+
+    void Update ()
+    {
+        text.text = "Score: " + score;
+    }
+}
